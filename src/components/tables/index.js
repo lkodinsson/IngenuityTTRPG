107530405915778ed836a0bca291d3982456b704
Ben Allison <ben1120@gmail.com>
2025-12-02 22:43:54 -0800

    Add Tooltip for features, embed in YAMLTable desc (#8)

---
+++ src/components/tables/index.js
@@ -0,0 +1,29 @@
+import proficiencies from "@site/src/components/tables/proficiencies.yml";
+import conditions from "@site/src/components/tables/conditions.yml";
+
+export {
+  // re-exporting for easy access elsewhere
+  proficiencies,
+  conditions,
+};
+
+export const proficienciesMap = proficiencies.reduce((acc, item) => {
+  acc[item.name.toLowerCase()] = item;
+  return acc;
+}, {});
+export const conditionsMap = conditions.reduce((acc, item) => {
+  acc[item.name.toLowerCase()] = item;
+  return acc;
+}, {});
+
+export const allFeaturesMap = {
+  ...proficienciesMap,
+  ...conditionsMap,
+};
+
+export const allFeatureNames = Object.keys(allFeaturesMap);
+
+export const allFeatureNamesRegex = new RegExp(
+  `\\b(${allFeatureNames.join("|")})\\b`,
+  "gi"
+);
